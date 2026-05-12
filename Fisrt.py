eb8be6bd92b7325a1c9ba3262d5a42a5ff9780ed
mkamilali <kamilalillc@gmail.com>
2026-05-12 15:08:09 +0500

    Python List Program

---
+++ Fisrt.py
@@ -0,0 +1,24 @@
+Fruits=[]
+
+a=(input("Enter Fruits Names: "))
+Fruits.append(a)
+
+b=(input("Enter Fruits Names: "))
+Fruits.append(b)
+
+c=(input("Enter Fruits Names: "))
+Fruits.append(c)
+
+d=(input("Enter Fruits Names: "))
+Fruits.append(d)
+
+e=(input("Enter Fruits Names: "))
+Fruits.append(e)
+
+f=(input("Enter Fruits Names: "))
+Fruits.append(f)
+
+g=(input("Enter Fruits Names: "))
+Fruits.append(g)
+
+print(Fruits)
